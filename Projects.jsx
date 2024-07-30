66be49efa2e6f90c2bad58cc039a4f2c0373b810
SelvaWeb <selvaraj.openweb@gmail.com>
2024-07-30 13:33:23 +0530

    Add files via upload

---
+++ Projects.jsx
@@ -0,0 +1,45 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/RamanasLogo.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/SelvaWeb/MyOne.git"
+          h3="Ramanas Food Production"
+          p="Product visibility App"
+        />
+        {/*  
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+
+         */}
+        <ProjectCard
+          src={fitLift}
+          link=""
+          h3="Janaredtas"
+          p="QR code janareat App"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
