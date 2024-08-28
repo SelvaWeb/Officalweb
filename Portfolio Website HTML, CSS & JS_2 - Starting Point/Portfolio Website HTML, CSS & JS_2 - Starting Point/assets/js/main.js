0f1af87bb459609c58adf505b2a18d70b349cfad
SelvaWeb <selvaraj.openweb@gmail.com>
2024-08-28 21:59:59 +0530

    Add files via upload

---
+++ Portfolio Website HTML, CSS & JS_2 - Starting Point/Portfolio Website HTML, CSS & JS_2 - Starting Point/assets/js/main.js
@@ -0,0 +1,40 @@
+// Dark Mode
+
+
+// Where User clicks on the menu button - open Menu
+
+
+
+// Where User clicks on the close(X) button - close Menu
+
+// Modal box must be shown on small devices only - not on desktop or large viewport
+
+
+
+// Get button that opens the modal
+
+
+// Get button that closes the modal
+
+
+// Get the media query of the viewport
+
+
+
+
+// Function to allow the modal box to be visible if they match the media query
+
+
+        // Close the modal when user clicks the close btn
+        
+
+        // Close the modal when user clicks the anywhere outside the modal
+
+
+
+
+// Add event listener
+
+
+
+// Close the side navbar where user click on the link
